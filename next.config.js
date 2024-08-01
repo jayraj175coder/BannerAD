7f22a0a2ba0e7b9c703616695a2ac99f4747e2d1
jayraj175coder <jayrajsanas175@gmail.com>
2024-08-01 09:39:13 +0530

    final

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true, // Example configuration, adjust as needed
+};
+
+module.exports = nextConfig;
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+// src/types.ts
+export interface AdBanner {
+    title: string;
+    description: string;
+    cta: string;
+    image: string;
+    background: string;
+  }
+  
